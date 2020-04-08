23cd843ea1f339edb069c651af772018f6443759
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-04-08 14:01:44 -0400

    Updated to use version 2.0.0 of Golant Client Library for Elastos Smartweb Service

--- webapp/edit.go
+++ webapp/edit.go
@@ -46,9 +46,71 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 
 const helloPlayground = `package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/cyber-republic/go-grpc-adenine/elastosadenine"
+	"github.com/cyber-republic/go-grpc-adenine/elastosadenine/stubs/health_check"
+)
+
+const (
+	// This is the shared secret that you need to generate an API key or get an API key
+	// This value is the same for both the gRPC client and the gRPC server
+	sharedSecretAdenine string = "7XDnFBdHafpPyIC4nrtuJ5EUYVqdEKjW"
+)
 
 func main() {
-	fmt.Println("Hello, Nucleus Go Playground")
+	fmt.Println("Hello, Nucleus Go Playground. This is a simple example of how to interact with Elastos Smartweb Service.")
+
+ 	grpcServerHost := "192.168.1.23" // Replace this with the host/IP where Elastos Smartweb Service is running
+    grpcServerPort := 8001			// Replace this with the port where Elastos Smartweb Service is running
+    production := false				// Set this to True if Elastos Smartweb Service uses TLS/SSL
+    
+    didToUse := "iHdasfhasdflkHdasfasdfD"
+
+	fmt.Println("--> Checking the health status of gRPC server(Elastos Smartweb Service)")
+	healthCheckTest(grpcServerHost, grpcServerPort, production)
+
+	fmt.Println("--> Generate API Key - SHARED_SECRET_ADENINE")
+	generateAPIKeyDemo(grpcServerHost, grpcServerPort, production, didToUse)
+
+	fmt.Println("--> Get API Key - SHARED_SECRET_ADENINE")
+	getAPIKeyDemo(grpcServerHost, grpcServerPort, production, didToUse)
+}
+
+func healthCheckTest(grpcServerHost string, grpcServerPort int, production bool) {
+	healthCheck := elastosadenine.NewHealthCheck(grpcServerHost, grpcServerPort, production)
+	defer healthCheck.Close()
+
+	response := healthCheck.Check()
+	if response.Status != health_check.HealthCheckResponse_SERVING {
+		fmt.Println("grpc server is not running properly")
+	} else {
+		fmt.Println("grpc server is running")
+	}
+}
+
+func generateAPIKeyDemo(grpcServerHost string, grpcServerPort int, production bool, didToUse string) {
+	common := elastosadenine.NewCommon(grpcServerHost, grpcServerPort, production)
+	defer common.Close()
+
+	response := common.GenerateAPIRequest(sharedSecretAdenine, didToUse)
+	if response.Status {
+		fmt.Printf("Output: %s\n", response.Output)
+	} else {
+		fmt.Printf("Error Message: %s\n", response.StatusMessage)
+	}
+}
+
+func getAPIKeyDemo(grpcServerHost string, grpcServerPort int, production bool, didToUse string) {
+	common := elastosadenine.NewCommon(grpcServerHost, grpcServerPort, production)
+	defer common.Close()
+
+	response := common.GetAPIKey(sharedSecretAdenine, didToUse)
+	if response.Status {
+		fmt.Printf("Output: %s\n", response.Output)
+	} else {
+		fmt.Printf("Error Message: %s\n", response.StatusMessage)
+	}
 }
 `
